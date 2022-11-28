1b0883937e8d1eec3899d5a655e9270b41f1ddeb
Dusuna <dusunax@gmail.com>
2022-11-28 16:56:38 +0900

    [Fix] vercel 빌드: import 대소문자 수정

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Member from './member/Member';
 import Login from './login/Index';
-import CustomFish from './customFishbread/customFish';
+import CustomFish from './customFishbread/CustomFish';
 import List from './fishBreadList/List';
 
 function Router() {
@@ -9,8 +9,8 @@ function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/:uid" element={<Member />}/>
-        <Route path="/customFish/:uid" element={<CustomFish />}/>
+        <Route path="/:uid" element={<Member />} />
+        <Route path="/customFish/:uid" element={<CustomFish />} />
         <Route path="/list/:uid" element={<List />} />
       </Routes>
     </BrowserRouter>
